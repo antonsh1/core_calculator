59e335caf6f5a618444859831d36e394e72a5211
Shelepov Anton <antonsh1@mail.ru>
2023-01-31 09:30:52 +0500

    Изменение multiply при делении на 0

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(30, 2);
+
         //Здесь значение не должно быть равно 0
-        int b = calc.minus.apply(1,5);
+        int b = calc.minus.apply(1, 1);
+
         int c = calc.divide.apply(a, b);
-        int d = calc.multiply.apply(b,c);
+        int d = calc.multiply.apply(b, c);
         int e = calc.pow.apply(d);
         int f = calc.abs.apply(c);
+
+        boolean isFPositive = calc.isPositive.test(f);
+        boolean isCPositive = calc.isPositive.test(c);
         calc.printing.out("a", a);
         calc.printing.out("b", b);
         calc.printing.out("c", c);
         calc.printing.out("d", d);
         calc.printing.out("e", e);
         calc.printing.out("f", f);
-        calc.printing.out("Положительное f", calc.isPositive.test(f));
-        calc.printing.out("Положительное c", calc.isPositive.test(c));
+        calc.printing.out("Положительное f", isFPositive);
+        calc.printing.out("Положительное c", isCPositive);
 
     }
 }
--- src/classes/Calculator.java
+++ src/classes/Calculator.java
@@ -2,28 +2,26 @@ package classes;
 
 import interfaces.Printing;
 
-import java.util.function.*;
-
-import static java.lang.Float.NaN;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Calculator {
     public static Supplier<Calculator> instance = Calculator::new;
 
     public Calculator() {
+
     }
 
     public BinaryOperator<Integer> plus = (x, y) -> x + y;
     public BinaryOperator<Integer> minus = (x, y) -> x - y;
     public BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    public BinaryOperator<Integer> divide = (x, y) -> x / y;
+    public BinaryOperator<Integer> divide = (x, y) -> (int) ((float) x / (float) y);
     public UnaryOperator<Integer> pow = x -> x * x;
     public UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     public Predicate<Integer> isPositive = x -> x > 0;
 
-    public Printing<String, Object> printing = (a, b) -> {
-        System.out.println(a + " : " + b);
-    };
-
-
+    public Printing<String, Object> printing = (a, b) -> System.out.println(a + " : " + b);
 
 }
